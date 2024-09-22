dbd2614237748b2991171083727233cec4717e75
flagrantii <tanuson679@gmail.com>
2024-09-22 21:36:11 +0700

    fix : id to be string

---
+++ prisma/migrations/20240922140023_init/migration.sql
@@ -0,0 +1,123 @@
+/*
+  Warnings:
+
+  - The primary key for the `Address` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `File` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Order` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `OrderLine` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Product` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `ProductCategory` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Address" DROP CONSTRAINT "Address_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "File" DROP CONSTRAINT "File_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "File" DROP CONSTRAINT "File_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderLine" DROP CONSTRAINT "OrderLine_orderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderLine" DROP CONSTRAINT "OrderLine_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Payment" DROP CONSTRAINT "Payment_orderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Product" DROP CONSTRAINT "Product_categoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Product" DROP CONSTRAINT "Product_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Address" DROP CONSTRAINT "Address_pkey",
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Address_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Address_id_seq";
+
+-- AlterTable
+ALTER TABLE "File" DROP CONSTRAINT "File_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ALTER COLUMN "productId" SET DATA TYPE TEXT,
+ALTER COLUMN "categoryId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "File_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "File_id_seq";
+
+-- AlterTable
+ALTER TABLE "Order" DROP CONSTRAINT "Order_pkey",
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Order_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Order_id_seq";
+
+-- AlterTable
+ALTER TABLE "OrderLine" DROP CONSTRAINT "OrderLine_pkey",
+ALTER COLUMN "productId" SET DATA TYPE TEXT,
+ALTER COLUMN "orderId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "OrderLine_pkey" PRIMARY KEY ("orderId", "productId");
+
+-- AlterTable
+ALTER TABLE "Payment" ALTER COLUMN "orderId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Product" DROP CONSTRAINT "Product_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "categoryId" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Product_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Product_id_seq";
+
+-- AlterTable
+ALTER TABLE "ProductCategory" DROP CONSTRAINT "ProductCategory_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "ProductCategory_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "ProductCategory_id_seq";
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "ProductCategory"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderLine" ADD CONSTRAINT "OrderLine_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderLine" ADD CONSTRAINT "OrderLine_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
