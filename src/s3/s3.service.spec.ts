dbf95349949c7f127b23727bf53589bc4e6825e9
flagrantii <tanuson679@gmail.com>
2024-09-24 00:07:04 +0700

    refector : s3 test

--- src/s3/s3.service.spec.ts
+++ src/s3/s3.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { S3Service } from './s3.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('S3Service', () => {
   let service: S3Service;
 
+  const mockConfigService = {
+    get: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [S3Service],
+      providers: [S3Service, { provide: ConfigService, useValue: mockConfigService }],
     }).compile();
 
     service = module.get<S3Service>(S3Service);
