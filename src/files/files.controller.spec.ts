d2d6e0664bff09776abeb23934d1b7cce9cd201e
flagrantii <tanuson679@gmail.com>
2024-09-24 00:06:25 +0700

    refector : file test

--- src/files/files.controller.spec.ts
+++ src/files/files.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilesController } from './files.controller';
+import { FilesService } from './files.service';
 
 describe('FilesController', () => {
   let controller: FilesController;
 
+  const mockFilesService = {
+    uploadFile: jest.fn(),
+    deleteFile: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FilesController],
+      providers: [
+        FilesService,
+        {
+          provide: FilesService,
+          useValue: mockFilesService,
+        },
+      ],
     }).compile();
 
     controller = module.get<FilesController>(FilesController);
--- src/files/files.service.spec.ts
+++ src/files/files.service.spec.ts
@@ -1,12 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilesService } from './files.service';
+import { DatabaseService } from '../database/database.service';
+import { S3Service } from '../s3/s3.service';
 
 describe('FilesService', () => {
   let service: FilesService;
 
+  const mockDatabaseService = {
+    file: {
+      create: jest.fn(),
+      findMany: jest.fn(),
+      findUnique: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    },
+  };
+
+  const mockS3Service = {
+    uploadFile: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FilesService],
+      providers: [
+        FilesService,
+        {
+          provide: DatabaseService,
+          useValue: mockDatabaseService,
+        },
+        {
+          provide: S3Service,
+          useValue: mockS3Service,
+        },
+      ],
     }).compile();
 
     service = module.get<FilesService>(FilesService);
